c43393294824a43cff53be54fbeeb17c22eac378
Zebediah Allton <zebediah.allton@gmail.com>
2025-09-28 13:35:12 -0500

    Finished all main and extended objectives for block20.

---
+++ index.js
@@ -0,0 +1,153 @@
+// === Constants ===
+const BASE = "https://fsa-crud-2aa9294fe819.herokuapp.com/api";
+const COHORT = "/2508-FTB-ET-WEB-FT";
+const API = BASE + COHORT;
+
+// === State ===
+let events = [];
+let selectedEvent;
+let guests = [];
+let rsvps = [];
+
+// State function to update state by fetching an array of events
+// from the API.
+async function getEvents() {
+  try {
+    const res = await fetch(API + "/Events");
+    const json = await res.json();
+    events = json.data;
+    render();
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+// State function to update state by fetching a single event
+// from the API.
+async function getEvent(id) {
+  try {
+    const res = await fetch(`${API}/Events/` + id);
+    const json = await res.json();
+    selectedEvent = json.data;
+    render();
+  } catch (err) {
+    console.log(err);
+  }
+}
+// State function to update state by fetching an array of guests
+// from the API
+async function getGuests() {
+  try {
+    const res = await fetch(API + "/guests");
+    const json = await res.json();
+    guests = json.data;
+    render();
+  } catch (err) {
+    console.log(err);
+  }
+}
+// State function to update state by fetching an array of RSVPs
+// from the API
+async function getRSVPs() {
+  try {
+    const res = await fetch(API + "/rsvps");
+    const json = await res.json();
+    rsvps = json.data;
+    render();
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+// === Components ===
+
+// function with event listener when party name is clicked on
+// the application updates state by fetching information about a
+// single part from the API.
+function EventListItem(event) {
+  const $li = document.createElement("li");
+  $li.innerHTML = `<a href = #selected>${event.name}</a>`;
+  $li.addEventListener("click", async function () {
+    await getEvent(event.id);
+  });
+  return $li;
+}
+
+// Function to render list of party names
+function EventList() {
+  const $ul = document.createElement("ul");
+  $ul.classList.add("lineup");
+  const $events = events.map(EventListItem);
+  $ul.replaceChildren(...$events);
+  return $ul;
+}
+
+// Function to render name, ID, date, description, and location
+// of selectied party.
+function EventDetails() {
+  if (!selectedEvent) {
+    const $p = document.createElement("p");
+    $p.textContent = "Please select a party to know more.";
+    return $p;
+  }
+
+  const $section = document.createElement("section");
+  $section.classList.add("event");
+  $section.innerHTML = `
+    <h3>${selectedEvent.name} #${selectedEvent.id}</h3>
+        <time datetime = "${selectedEvent.date}">
+        ${selectedEvent.date.slice(0, 10)}
+        </time>
+        <address>${selectedEvent.location}</address>
+        <p>${selectedEvent.description}</p>
+        <GuestList></GuestList>`;
+
+  $section.querySelector("GuestList").replaceWith(GuestList());
+  console.log(GuestList());
+  return $section;
+}
+// Function that filters the guests with who RSVP'd to which event.
+function GuestList() {
+  const $ul = document.createElement("ul");
+  const eventGuestRsvp = guests.filter((guest) =>
+    rsvps.find(
+      (rsvp) => rsvp.guestId === guest.id && rsvp.eventId === selectedEvent.id
+    )
+  );
+  const $guests = eventGuestRsvp.map((guest) => {
+    const $guest = document.createElement("li");
+    $guest.textContent = guest.name;
+    return $guest;
+  });
+  $ul.replaceChildren(...$guests);
+  return $ul;
+}
+
+// === Render ===
+function render() {
+  const $app = document.querySelector("#app");
+  $app.innerHTML = `
+    <h1>Party Planner</h1>
+    <main>
+      <section class = "upcoming">
+        <h2>Upcoming Parties</h2>
+        <EventList></EventList>
+      </section>
+      <section class = "details" id="selected">
+        <h2>Party Details</h2>
+        <EventDetails></EventDetails>
+      </section>
+    </main>
+  `;
+  $app.querySelector("EventList").replaceWith(EventList());
+  $app.querySelector("EventDetails").replaceWith(EventDetails());
+}
+
+async function initialize() {
+  await getEvents();
+  await getGuests();
+  await getRSVPs();
+  render();
+}
+
+initialize();
